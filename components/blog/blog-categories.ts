1abe5c6e5ef49bfd3daf74b01b2c60fc264f753f
boutiqueambiancejapon-sketch <boutiqueambiancejapon@gmail.com>
2026-06-11 13:38:12 +0200

    feat(article+mapping): FAQ santé kiwi + slot ARTICLE_SLOT_BY_SLUG

--- components/blog/blog-categories.ts
+++ components/blog/blog-categories.ts
@@ -176,6 +176,7 @@ const ARTICLE_SLOT_BY_SLUG: Record<string, string> = {
   'chien-peut-manger-noix': 'art-chien-peut-manger-noix',
   'chien-peut-manger-lentilles': 'art-chien-peut-manger-lentilles',
   'recette-poulet-seche-chien': 'art-recette-poulet-seche-chien',
+  'chien-peut-manger-kiwi': 'art-chien-peut-manger-kiwi',
 }
 
 /**
