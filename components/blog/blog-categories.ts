5724162d6e7e75e6c7840345abaadbf4331dd3bf
boutiqueambiancejapon-sketch <boutiqueambiancejapon@gmail.com>
2026-08-19 01:17:54 +0200

    feat(blog): map article slug chien-peut-boire-lait to dedicated featured slot

--- components/blog/blog-categories.ts
+++ components/blog/blog-categories.ts
@@ -233,6 +233,7 @@ const ARTICLE_SLOT_BY_SLUG: Record<string, string> = {
   'recette-biscuits-pomme-cannelle-chien': 'art-recette-biscuits-pomme-cannelle-chien',
   'alimentation-chien-grande-race-adulte-guide': 'art-alimentation-chien-grande-race-adulte-guide',
   'nourriture-braque-hongrois': 'art-nourriture-braque-hongrois',
+  'chien-peut-boire-lait': 'art-chien-peut-boire-lait',
 }
 
 /**
