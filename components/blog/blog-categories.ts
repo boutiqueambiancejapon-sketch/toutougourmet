db0a6c39bf3a406cb6a148aee7e77163e86c5292
boutiqueambiancejapon-sketch <boutiqueambiancejapon@gmail.com>
2026-08-21 01:32:10 +0200

    feat(blog): map article slug recette-patee-maison-agneau-chien to dedicated featured slot

--- components/blog/blog-categories.ts
+++ components/blog/blog-categories.ts
@@ -198,6 +198,7 @@ const ARTICLE_SLOT_BY_SLUG: Record<string, string> = {
   'recette-patee-maison-poulet-chien': 'art-recette-patee-maison-poulet-chien',
   'recette-patee-maison-poisson-chien': 'art-recette-patee-maison-poisson-chien',
   'recette-patee-maison-boeuf-chien': 'art-recette-patee-maison-boeuf-chien',
+  'recette-patee-maison-agneau-chien': 'art-recette-patee-maison-agneau-chien-v2',
   'chien-peut-manger-pomme': 'art-chien-peut-manger-pomme',
   'gamelle-surelevee-chien': 'art-gamelle-surelevee-chien-v2',
   'recette-patate-douce-sechee-chien': 'art-recette-patate-douce-sechee-chien',
